b178febc5ce1cb5debfc0b175f7cf03b5e06e765
Muhammad Saqib Ilyas <msaqib@gmail.com>
2023-09-29 19:27:39 +0500

    Deep learning text generation API not allowing any more text generation. Coded my own lorem ipsum text generator.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const imageurl = 'https://randomuser.me/api/?gender=female?results=20'
-const texturl = "https://api.deepai.org/api/text-generator"
+const length = 20
+const imageurl = 'https://randomuser.me/api/?gender=female?results=' + length 
+/*const texturl = "https://api.deepai.org/api/text-generator"
 const textQuery = "Generate a random customer review for a fast food joint"
 const formData = new FormData()
-formData.append('text', textQuery)
+formData.append('text', textQuery)*/
 
 const image = document.getElementById('image')
 const name = document.getElementById('name')
@@ -13,7 +14,46 @@ const backward = document.getElementById('backward')
 const loader = document.getElementById('loader')
 const reviewSection = document.querySelector('.review')
 
-const imageRequest = fetch(imageurl)/*.then(response => {
+const words = ['lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit', 'sed', 'do', 'eiusmod', 'tempor', 'incididunt', 'ut', 'labore', 'et', 'dolore', 'magna', 'aliqua'];
+let reviews = []
+
+const minWordsPerSentence = 5
+const maxWordsPerSentence = 15
+
+const minSentences = 1
+const maxSentences = 5
+
+function generateRandomSentence(numWords) {
+    let sentence = ''
+    for (let i = 0 ; i < numWords ; i++) {
+        const randIndex = Math.floor(Math.random()*words.length)
+        sentence += words[randIndex]
+        if(i < numWords - 1) {
+            sentence += ' '
+        }
+    }
+    sentence += '.'
+    return sentence
+}
+
+function generateRandomReview() {
+    let review = ''
+    const numSentences = Math.floor(Math.random()*(maxSentences - minSentences) + minSentences)
+    for (let i = 0 ; i < numSentences ; i++) {
+        const numWords = Math.floor(Math.random()*(maxWordsPerSentence - minWordsPerSentence) + minWordsPerSentence)
+        review += generateRandomSentence(numWords)
+        if (i < numSentences - 1) {
+            review += ' '
+        }
+    }
+    return review
+}
+
+for (let i = 0 ; i < length ; i++) {
+    reviews.push(generateRandomReview())
+}
+
+const imageRequest = fetch(imageurl).then(response => {
     if (response.ok) {
         return response.json()
     }
@@ -26,16 +66,18 @@ const imageRequest = fetch(imageurl)/*.then(response => {
     profession.textContent = 'Jahil'
     loader.style.visibility = 'hidden';
     loader.style.height = '0'
+        message.textContent = reviews[0]
     reviewSection.style.visibility = 'visible'
-}).catch(error => console.log(error))*/
+}).catch(error => console.log(error))
 
-const textRequest = fetch(texturl, {
+/*const textRequest = fetch(texturl, {
     method: 'POST',
     headers: {
         'api-key': 'quickstart-QUdJIGlzIGNvbWluZy4uLi4K',
     },
     body: formData,
-})/*.then(response => {
+})
+.then(response => {
     if(response.ok) {
         return response.json()
     }
@@ -46,7 +88,7 @@ const textRequest = fetch(texturl, {
     console.log(data.output)
 }).catch(error => console.log(error))*/
 
-Promise.all([imageRequest, textRequest]).then(responses => {
+/*Promise.all([imageRequest, textRequest]).then(responses => {
     if (responses.every(response => response.status === 200)) {
         return Promise.all(responses.map(response => response.json()))
     }
@@ -64,4 +106,6 @@ Promise.all([imageRequest, textRequest]).then(responses => {
     reviewSection.style.visibility = 'visible'
 
     message.textContent = textResponse.output
-}).catch(error => console.log(error))
+}).catch(error => console.log(error))*/
+
+
